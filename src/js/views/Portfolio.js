58240dd26ffded63df0c956aa9819b935e52e652
rekaens <149834281+rekaens@users.noreply.github.com>
2024-12-21 18:40:57 +0100

    feat: remove chart when screen md

--- src/js/views/Portfolio.js
+++ src/js/views/Portfolio.js
@@ -3,8 +3,6 @@ import React from "react";
 import { withBaseLayout } from "../layout/Base";
 
 // components
-import ShoeCard from "../components/ShoeCard";
-import PortfolioNavbar from "../components/portfolio/PortofolioNavbar";
 import PortfolioChart from "../components/portfolio/PortfolioChart";
 import PortfolioList from "../components/portfolio/PortfolioList";
 
@@ -12,10 +10,10 @@ function Portfolio() {
     return (
         <div className="bg-green-200 h-full flex flex-col">
             <div className="bg-blue-200 flex-1 flex p-3">
-                <div className="w-1/3 bg-red-400 me-2">
+                <div className="hidden md:block md:w-1/3 bg-red-400 me-2">
                     <PortfolioChart />
                 </div>
-                <div className="w-2/3 bg-gray-400 ms-2">
+                <div className="w-full md:w-2/3 bg-gray-400 ms-2">
                     <PortfolioList />
                 </div>
             </div>
